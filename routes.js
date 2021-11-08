2451b573ed9cb11478085ab58a0fb5a78c320281
Xar0P <pietroricardocres@hotmail.com>
2021-11-08 18:19:57 -0300

    login template

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const route = express.Router();
-const homeController = require('./src/controllers/homeController');
 
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+route.get('/', homeController.index);
+
+route.get('/login/', loginController.index);
 
 module.exports = route;
