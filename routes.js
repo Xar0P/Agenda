1d827960bebb69c2c661fd95a50615a769455ba8
Pietro <pietroricardocres@hotmail.com>
2021-11-20 23:47:43 -0300

    contact register

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 route.get('/', homeController.index);
 
@@ -10,5 +13,9 @@ route.get('/login/', loginController.index);
 route.post('/login/register/', loginController.register);
 route.post('/login/login/', loginController.login);
 route.get('/login/logout/', loginController.logout);
+
+route.get('/contato/', loginRequired, contatoController.index);
+route.post('/contato/register', contatoController.register);
+route.get('/contato/:id', contatoController.editIndex);
 
 module.exports = route;
